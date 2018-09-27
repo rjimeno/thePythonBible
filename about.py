3172d82aae763d5fd1d2ca4a200081ccafeeb90f
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-09-27 15:16:47 -0400

    initial commit.

---
+++ about.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Create a variable called name and set it equal to your name
+name = "Ro"
+
+# Create a variable called age and set it to your age in years
+# Note: This must be an int, not a string!
+age = 46
+
+# Create a variable called place and set it equal to where you live
+place = "Brooklyn"
+
+# Create a variable called output, and use string formatting to
+# make it like the example text in the description
+output = "Hello my name is {} and I am {} years old. I live in {} and I love Python!".format(name, age, place)
+
+# Finally, use the print() function to print the output.
+print(output)
