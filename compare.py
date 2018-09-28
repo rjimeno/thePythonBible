7f43aa73c96b06fff55c10cafe4865a1dce8be85
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-09-28 01:41:16 -0400

    initial commit.

---
+++ compare.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+NUM1 = 11
+NUM2 = 13
+
+if NUM1 > NUM2:  # Replace CONDITION1 with a valid condition that makes this work
+    print("NUM1 is bigger than NUM2")
+
+elif NUM1 == NUM2:  # Replace CONDITION2 with a valid condition that make this work
+    print("NUM1 is equal to NUM2")
+    
+elif NUM2 > NUM1:  # Replace CONDITION3 with a valid condition that make this work
+    print("NUM2 is bigger than NUM1")
