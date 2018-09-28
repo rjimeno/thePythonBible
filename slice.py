41d941e1dcb4dbe252b4fcffa3c6e31f3c4ef6ab
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-09-28 00:58:45 -0400

    initial commit.

---
+++ slice.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#  here is a very long word
+word = "antidisestablishmentarianism"
+
+# use a slice to take out the word "establishment"
+# and store it in the answer variable....
+answer = word[7:-8]
