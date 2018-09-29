521d420cb8a4456907954c81bf7e89ca0836f681
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-09-28 20:24:39 -0400

    initial commit.

---
+++ meeting_idle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Section 2, Lecture 5. Meeting IDLE.
+"""
+
+FIRST_NUMBER = 1 + 1
+print(FIRST_NUMBER)
+SECOND_NUMBER = 105 + 10
+print(SECOND_NUMBER)
+
+TOTAL = FIRST_NUMBER + SECOND_NUMBER
+
+print(TOTAL)
